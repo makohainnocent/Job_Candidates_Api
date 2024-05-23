1e303bff0e83af513c5e48d539457e9ebf817bfc
makoha innocent <vantagepoint2019@outlook.com>
2024-05-24 02:59:47 +0300

    feat: added dbconnection class plus installed sqlite package and configuration package

---
+++ Domain/Models/DbConnection.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Domain.Models
+{
+    public class DbConnection
+    {
+        private readonly IConfiguration _configuration;
+
+        public DbConnection(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+            return new SqlConnection(connectionString);
+        }
+    }
+}
