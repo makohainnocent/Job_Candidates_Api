e80229d0393cc670bb64d34128e00e82bef21a4a
makoha innocent <vantagepoint2019@outlook.com>
2024-05-24 01:27:15 +0300

    feat: add Candidate class with validation attributes
    - Created Candidate class with properties: Id, FirstName, LastName, Email, PhoneNumber, PreferredCallTime, LinkedInProfileUrl, GitHubProfileUrl, and FreeTextComment.
    - Added data annotations to enforce validation rules on required fields: FirstName, LastName, Email, and FreeTextComment.
    - Included additional validation for Email and URL fields.

---
+++ Domain/Models/Candidate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Candidate
+    {
+        public int Id { get; set; }
+
+        
+        [Required(ErrorMessage = "FirstName is required.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "LastName is required.")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Comment is required.")]
+        public string FreeTextComment { get; set; }
+
+        
+        public string? PhoneNumber { get; set; } 
+
+        public string? PreferredCallTime { get; set; } 
+
+        [Url(ErrorMessage = "Invalid LinkedIn profile URL.")]
+        public string? LinkedInProfileUrl { get; set; } 
+
+        [Url(ErrorMessage = "Invalid GitHub profile URL.")]
+        public string? GitHubProfileUrl { get; set; } 
+    }
+}
