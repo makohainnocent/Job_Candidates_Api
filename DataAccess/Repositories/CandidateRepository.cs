24d3ff3cf1836ee06c1c8c4ba17cac1b636d428d
makoha innocent <vantagepoint2019@outlook.com>
2024-05-24 02:14:20 +0300

    feat: add CandidateRepository class
    - Created the CandidateRepository class to implement data access logic for candidate entities.
    - The repository class will handle CRUD operations related to candidates.

---
+++ DataAccess/Repositories/CandidateRepository.cs
@@ -0,0 +1,14 @@
+﻿using Application.
+using Application.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public class CandidateRepository:ICandidateRepository
+    {
+    }
+}
