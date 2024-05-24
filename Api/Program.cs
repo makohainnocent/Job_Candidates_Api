68a0ae01512767399ef81b934daf8aad5fe10781
makoha innocent <vantagepoint2019@outlook.com>
2024-05-24 19:26:02 +0300

    feat: setup routing

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,8 +1,19 @@
+using Application.Abstractions;
+using DataAccess.DbConnection;
+using DataAccess.Repositories;
+using Microsoft.AspNetCore.Connections;
+using SQLitePCL;
+
+Batteries.Init();
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IDbConnectionProvider, DbConnection>();
+builder.Services.AddTransient<ICandidateRepository, CandidateRepository>();
 
 var app = builder.Build();
 
@@ -13,6 +24,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
+
+app.MapGet("/api/candidates/{id}", async (ICandidateRepository candidateRepository,int id) => { 
+    var candidate = await candidateRepository.GetCandidate(id);
+    return Results.Ok(candidate);
+
+});
 
 app.Run();
 
