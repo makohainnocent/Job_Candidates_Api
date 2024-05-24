5b3a7c0dded1f8141e4f5495472564e939b670d5
makoha innocent <vantagepoint2019@outlook.com>
2024-05-24 03:13:33 +0300

    feat: added interface IDbConnectionProvider

---
+++ Application/Abstractions/IDbConnectionProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Abstractions
+{
+    public interface IDbConnectionProvider
+    {
+        public interface IDbConnectionProvider
+        {
+            IDbConnection CreateConnection();
+        }
+    }
+}
