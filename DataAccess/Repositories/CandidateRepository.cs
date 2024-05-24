f2c5c99f679fd2f2380cdfcdf92f2778be5d4d76
makoha innocent <vantagepoint2019@outlook.com>
2024-05-24 10:40:42 +0300

    feat: implemented some of the members in candidate repository

--- DataAccess/Repositories/CandidateRepository.cs
+++ DataAccess/Repositories/CandidateRepository.cs
@@ -1,33 +1,86 @@
 ﻿using Application.Abstractions;
 using Domain.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Data;
 using System.Threading.Tasks;
+using Dapper;
+using System.Linq;
 
 namespace DataAccess.Repositories
 {
     public class CandidateRepository : ICandidateRepository
     {
-        public Task<Candidate> CreateCandidate(Candidate candidate)
+        private readonly IDbConnectionProvider _dbConnectionProvider;
+
+        public CandidateRepository(IDbConnectionProvider dbConnectionProvider)
         {
-            throw new NotImplementedException();
+            _dbConnectionProvider = dbConnectionProvider;
         }
 
-        public Task DeleteCandidate(int id)
+        public async Task<Candidate> CreateCandidate(Candidate candidate)
         {
-            throw new NotImplementedException();
+            const string sql = @"
+                INSERT INTO Candidates (FirstName, LastName, Email, PhoneNumber, PreferredCallTime, LinkedInProfileUrl, GitHubProfileUrl, FreeTextComment)
+                VALUES (@FirstName, @LastName, @Email, @PhoneNumber, @PreferredCallTime, @LinkedInProfileUrl, @GitHubProfileUrl, @FreeTextComment);
+                SELECT LAST_INSERT_ID();";
+
+            using (var connection = _dbConnectionProvider.CreateConnection())
+            {
+                var candidateId = await connection.QueryFirstOrDefaultAsync<int>(sql, candidate);
+                candidate.Id = candidateId;
+                return candidate;
+            }
         }
 
-        public Task<Candidate> GetCandidate(int id)
+        public async Task DeleteCandidate(int id)
         {
-            throw new NotImplementedException();
+            const string sql = "DELETE FROM Candidates WHERE Id = @Id";
+
+            using (var connection = _dbConnectionProvider.CreateConnection())
+            {
+                await connection.ExecuteAsync(sql, new { Id = id });
+            }
         }
 
-        public Task<ICollection<Candidate>> GetCandidates()
+        public async Task<Candidate> GetCandidate(int id)
         {
-            throw new NotImplementedException();
+            const string sql = "SELECT * FROM Candidates WHERE Id = @Id";
+
+            using (var connection = _dbConnectionProvider.CreateConnection())
+            {
+                return await connection.QueryFirstOrDefaultAsync<Candidate>(sql, new { Id = id });
+            }
+        }
+
+        public async Task<ICollection<Candidate>> GetCandidates()
+        {
+            const string sql = "SELECT * FROM Candidates";
+
+            using (var connection = _dbConnectionProvider.CreateConnection())
+            {
+                return (await connection.QueryAsync<Candidate>(sql)).ToList();
+            }
+        }
+
+        public async Task<Candidate> UpdateCandidate(Candidate candidate)
+        {
+            const string sql = @"
+                UPDATE Candidates 
+                SET FirstName = @FirstName, 
+                    LastName = @LastName, 
+                    Email = @Email, 
+                    PhoneNumber = @PhoneNumber, 
+                    PreferredCallTime = @PreferredCallTime, 
+                    LinkedInProfileUrl = @LinkedInProfileUrl, 
+                    GitHubProfileUrl = @GitHubProfileUrl, 
+                    FreeTextComment = @FreeTextComment
+                WHERE Id = @Id";
+
+            using (var connection = _dbConnectionProvider.CreateConnection())
+            {
+                await connection.ExecuteAsync(sql, candidate);
+                return await GetCandidate(candidate.Id);
+            }
         }
 
         public Task<Candidate> UpdateCandidate(string updatedContent, int id)
